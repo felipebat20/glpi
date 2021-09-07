e57b44d899f4bec6f7f9675ac1ab0e62c059bce9
= <=>
2021-09-06 23:21:35 -0300

    Remove os administradores da listagem

--- api/app/Http/Controllers/UserController.php
+++ api/app/Http/Controllers/UserController.php
@@ -14,25 +14,14 @@ class UserController extends Controller
      */
     public function index()
     {
-        return User::orderBy('created_at', 'DESC')->get();
+        return User::where('usertype', '<>', 3)->get();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $newUser = new User;
@@ -45,35 +34,16 @@ class UserController extends Controller
         return $newUser;
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         return User::find($id);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
        
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         $existingUser = User::find($id);
@@ -90,12 +60,6 @@ class UserController extends Controller
         return abort(404, 'User not found.');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         $existingUser = User::find($id);
